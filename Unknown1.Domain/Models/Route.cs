6aa798b6347a0be73962d087a6d9d954e88ea904
Mi7ai <hottmayer@gmail.com>
2025-03-19 11:45:13 +0100

    -added mockup for ui -modified domain models syntax

--- Unknown1.Domain/Models/Route.cs
+++ Unknown1.Domain/Models/Route.cs
@@ -1,22 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿    using System.ComponentModel.DataAnnotations;
 
-namespace Unknown1.Domain.Models
-{
-    public class Route
+    namespace Unknown1.Domain.Models
     {
-        public int RouteId { get; set; }
-        [Required]
-        public string? StartLocation { get; set; }
-        [Required]
-        public string? EndLocation { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime UpdatedAt { get; set; }
-        [Required]
-        public decimal Price { get; set; }
-        public decimal Discount { get; set; } = decimal.Zero;
-        public decimal TotalPrice { get; set; }
+        public class Route
+        {
+            public int RouteId { get; set; }
+            [Required]
+            public string? StartLocation { get; set; }
+            [Required]
+            public string? EndLocation { get; set; }
+            public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+            public DateTime UpdatedAt { get; set; }
+            [Required]
+            public decimal Price { get; set; }
+            public decimal Discount { get; set; } = decimal.Zero;
+            public decimal TotalPrice { get; set; }
 
-        // Navigation Property
-        public List<UserRoute> UserRoutes { get; set; } = [];
+            // Navigation Property
+            public List<UserRoute> UserRoutes { get; set; } = new List<UserRoute>();
+        }
     }
-}
